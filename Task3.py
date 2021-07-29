50700be7fbb5b05f1069bad6c95cc559d2285a76
MennaFayed <84094845+MennaFayed@users.noreply.github.com>
2021-07-29 17:32:16 +0200

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,55 @@
+#make a module contains 3 func and 4 const
+def divide2num(x,y):
+    print(x/y)
+    
+def print_alert_message():
+    print("try again"
+
+def find_even_oddnum():
+num = int(input("Enter a number: "))  
+if (num % 2) == 0:  
+   print("{0} is Even number".format(num))  
+else:  
+   print("{0} is Odd number".format(num)) 
+   
+def graduation_message():
+    print(“Warmest congratulations on your graduation.”)
+    
+
+def add question():
+    answer = input(" which language speak in egypt?")
+    if (answer == "arabic")
+        print("right")
+    else:
+        print("wrong")
+
+          
+constantvalue = 50
+speedoflight = 234134
+pi=3.14
+gravity=9.8
+#######create private constant
+public static final String CONST_NAME = "Name";
+          
+ def constant(f):
+    def fset(s, v):
+        raise TypeError
+    def fget(s):
+        return f()
+    return property(fget, fset)
+
+class _Const(object):
+    #constant
+    def Fo:
+        return 0xBAADFACE
+    #constant
+    def BAR():
+        return 0xDEADBEEF
+
+CONST = _Const()
+
+print CONST.FOO
+##3131964110
+
+CONST.FOO = 0         
+   
